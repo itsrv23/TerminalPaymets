3b874b61e5082a7f8df4e95ab1df3c65369dbae3
itsrv23 <101412457+itsrv23@users.noreply.github.com>
2023-01-24 11:51:18 +0300

    Initial commit

---
+++ src/main/java/ru/itsrv23/terminal/TerminalPaymentsApplication.java
@@ -0,0 +1,13 @@
+package ru.itsrv23.terminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminalPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/itsrv23/terminal/TerminalPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itsrv23.terminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
